461a19fefd2559f4909df8c2a85a440b12f35c20
guillaumeRequena <guillaume.r@ecotree.fr>
2024-08-03 20:36:05 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- 0) Header Component -->
+  <header>
+    <HeaderComponent />
+  </header>
+
+  <!-- <main> -->
+
+  <!-- TODO -> A REFLECHIR ->  pour les components de 1 à 4, les placer dans une div pour les apparitions de coté -->
+
+  <!-- 1) Exercices / Réalisations Component -->
+  <!-- Le plus difficile car du tri (back, front, framework) voir une recherche a mettre en place -->
+
+  <!-- 2) Formation Component -->
+
+  <!-- 3) Block centre d'interets (meme chose que technologies utilisées niveau css ?) -->
+
+  <!-- 4) Plus Component -->
+
+  <!-- 5) Modal Component -->
+
+  <!-- </main> -->
+
+  <!-- 6) Footer Component -->
+  <footer>
+    <FooterComponent />
+  </footer>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from "./components/shared/HeaderComponent";
+import FooterComponent from "./components/shared/FooterComponent";
+
+export default {
+  name: "App",
+  components: { HeaderComponent, FooterComponent },
+};
+</script>
+
+<style>
+/* TODO - use separate css file for @font-face and * selector */
+@font-face {
+  font-family: "comfortaa";
+  src: url("./assets/fonts/comfortaa/Comfortaa-Regular.woff2") format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+* {
+  padding: 0;
+  margin: 0;
+  font-size: 16px;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: "comfortaa", "Helvetica", "Arial", "sans-serif";
+  width: 100%;
+  /* TODO a virer le height car le header aura un 100 vh */
+  height: 100vh;
+  background-color: rgb(255, 250, 250);
+}
+
+header {
+  height: 100vh;
+  /* Gros probleme de style avec le retrait de l'image de fond et le fond blanc .... */
+  background-color: rgba(0, 0, 0, 0.4);
+  position: relative;
+}
+
+footer {
+  margin-top: 2rem;
+}
+</style>
---
+++ src/components/shared/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>
+    Guillaume Requena
+    <span>Développeur Web</span>
+  </h1>
+  <!-- Component logo ici ? -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  color: #fff;
+  font-size: 4rem;
+  text-align: center;
+  width: 90%;
+  position: absolute;
+  top: 35%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+h1 span {
+  font-size: 3rem;
+  display: block;
+  padding-top: 1rem;
+}
+
+/* TODO responsive */
+</style>
